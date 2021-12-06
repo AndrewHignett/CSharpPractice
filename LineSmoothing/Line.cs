9dd4af942a69ddf4c74c693bbee1ce550ec5ab0b
Pinguino25 <andrew.hignett@hotmail.co.uk>
2021-12-06 22:28:53 +0000

    Initial commit, basic plan outlined, Line class created

---
+++ LineSmoothing/Line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Line
+{
+    //dimensions of input image
+    public int x;
+    public int y;
+
+    //intention here is to reduce the lineto it's midmost parts, as this line will likely be more than 1 pixel wide
+    public void erode()
+    {
+
+    }
+
+    //need a constructure here
+    public Line()
+	{
+
+	}
+}
---
+++ LineSmoothing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineSmoothing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * read line from image
+             * smooth line
+             * output smoothed line
+             */
+        }
+    }
+}
